260a53adce3ad93dbca5b628b89e39d73520c98b
Whyu9-9 <wahyuivanmahendra@gmail.com>
2023-08-24 13:57:09 +0800

    add: refactor on folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import {
   Admin,
   Resource
 } from "react-admin";
-import { dataProvider } from "../providers/dataProvider";
-import { AdminList } from "../components/Admin/Admins";
-import { AdminCreate } from "../components/Admin/Create";
-import { AdminEdit } from "../components/Admin/Edit";
-import { EmployeeList } from "../components/Employee/Employees";
-import { EmployeeCreate } from "../components/Employee/Create";
-import { EmployeeEdit } from "../components/Employee/Edit";
-import { ProjectList } from "../components/Projects/Projects";
-import { ProjectCreate } from "../components/Projects/Create";
-import { ProjectEdit } from "../components/Projects/Edit";
-import ProjectShow from "../components/Projects/Show";
-import authProvider from '../providers/authProvider';
-import NotFound from '../components/misc/NotFound';
-import Dashboard from '../components/misc/Dashboard';
+import { dataProvider } from "./providers/dataProvider";
+import { AdminList } from "./pages/Admin/Admins";
+import { AdminCreate } from "./pages/Admin/Create";
+import { AdminEdit } from "./pages/Admin/Edit";
+import { EmployeeList } from "./pages/Employee/Employees";
+import { EmployeeCreate } from "./pages/Employee/Create";
+import { EmployeeEdit } from "./pages/Employee/Edit";
+import { ProjectList } from "./pages/Projects/Projects";
+import { ProjectCreate } from "./pages/Projects/Create";
+import { ProjectEdit } from "./pages/Projects/Edit";
+import ProjectShow from "./pages/Projects/Show";
+import authProvider from './providers/authProvider';
+import NotFound from './pages/misc/NotFound';
+import Dashboard from './pages/misc/Dashboard';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/People';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
